3a92ce1206a1535be5b9e8a1f35fbeb276c86fa9
Projektwoche2017 <timo.fuele@gmail.com>
2017-02-21 09:59:08 +0100

    Update functions.php

--- Projektwochenseite/Login Dateien/functions.php
+++ Projektwochenseite/Login Dateien/functions.php
@@ -128,7 +128,30 @@
 		require("pages/".$_GET["page"]."/config.php");
 		if (isset($backToTop)?$backToTop:false) {
 			echo '
-				//Timo here
+				<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js">
+					$(document).ready(function(){
+					var back_to_top_button = ['<a href="#top" class="back-to-top">Nach oben</a>'].join("");
+					$("body").append(back_to_top_button)
+					$(".back-to-top").hide();
+					$(function () {
+						$(window).scroll(function () {
+							if ($(this).scrollTop() > 100) { // Wenn 100 Pixel gescrolled wurde
+								$('.back-to-top').fadeIn();
+							} else {
+								$('.back-to-top').fadeOut();
+							}
+						});
+
+						$('.back-to-top').click(function () { // Klick auf den Button
+							$('body,html').animate({
+								scrollTop: 0
+							}, 800);
+							return false;
+						});
+					});
+
+				});
+				</script>
 			';
 		}
 	}
