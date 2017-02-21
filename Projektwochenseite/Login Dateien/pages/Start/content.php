522091a1410fa4bfac99e292a1f4bea7dd393a7a
TheRiotSnake <Beckermumi@web.de>
2017-02-21 10:53:53 +0100

    Update content.php

--- Projektwochenseite/Login Dateien/pages/Start/content.php
+++ Projektwochenseite/Login Dateien/pages/Start/content.php
@@ -1,3 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DANK</title>
+</head>
+<body>
 <section>
-	<h2 style="padding-left:1em">Willkommen, <?php echo $_SESSION['name'];?>!</h2>
+	<h1 class="center" style="padding-left:1em">Willkommen, <?php echo $_SESSION['name'];?>!</h1>
+	<div style="padding: 16px;
+		    border: 3px solid #f1f1f1;
+				max-width: 1008px;
+   	    	    margin: auto;">
+	<p>
+		Hier kannst du dich für Projekte eintragen. Bedenke, dass es eine 1., 2. und 3. Wahl gibt.
+	</p>
+	 <img
+	 
+	 style="width:500px; height:500px; padding:0; margin:o;"
+		     
+     src="pics/pic1.jpg" alt="Bild kann nicht geladen werden"></img>
+	 
+	 <img 
+
+	style="width:500px; height:500px; padding:0; margin:0;"
+	
+     src="pics/pic2.jpg" alt="Bild kann nicht geladen werden"></img>
+	 
+	 <img 
+
+	style="width:500px; height:500px; padding:0; margin:0;"
+	
+     src="pics/pic3.jpg" alt="Bild kann nicht geladen werden"></img>
+	 
+	 <img 
+
+	style="width:500px; height:500px; padding:0; margin:0;"
+	
+     src="pics/pic4.jpg" alt="Bild kann nicht geladen werden"></img>
+	</div>
 </section>
+</body>
+
+</html>
